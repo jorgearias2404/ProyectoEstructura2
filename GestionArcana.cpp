8384d47c5f1bc54a760aeaec06ff5a3bf86f7acb
Jorge Arias <ja549655@gmail.com>
2025-02-10 22:03:08 -0400

    Actualizacion se requieren pruebas

--- GestionArcana.cpp
+++ GestionArcana.cpp
@@ -2,215 +2,313 @@
 #include <fstream>
 using namespace std;
 
-
-//CADA VERTICE POSEE UNA LISTA DE LOS VERTICES ADYACENTES A EL
-
-class Nodo
-{
-private:
-    int valor;
-    float peso;
-    Nodo* siguiente;
-    Nodo* previo;
-public:
-   
-   //GETTERS
-   int getValor(){return valor;}
-   float getPeso(){return peso;}
-   Nodo* getSiguiente(){return siguiente;}
-   Nodo* getPrevio(){return previo;}
-
-   //setters
-   void setValor(int valor){this->valor = valor;}
-   void setPeso(float peso){this->peso = peso;}
-   void setSiguiente(Nodo* siguiente){this->siguiente = siguiente;}
-   void setPrevio(Nodo* previo){this->previo = previo;}
-   
-
-    Nodo(){
-        valor = 0;
-        peso = 0;
-        siguiente = nullptr;
-    }
-    
-};
-
-class ListDoble
-{
-private:
-   Nodo* head;
-   Nodo* tail;
-public:
-bool YaAgregado(int x){
-    Nodo* aux = head;
-    while (aux != nullptr) {
-        if (x == aux->getValor()) 
-        {
-            return true;
-        }
-        else{
-            aux = aux->getSiguiente();
-        }
-        
-    }
-    return false;
-}
-   void InsertarAlFinal(int X,float peso){
-    if (!YaAgregado(X))
-    {
-             Nodo* nuevoNodo = new Nodo();
-            nuevoNodo->setValor(X);
-            nuevoNodo->setPeso(peso);
-            nuevoNodo->setSiguiente(nullptr);
-            if (tail != nullptr) {
-                tail->setSiguiente(nuevoNodo);
-                nuevoNodo->setPrevio(tail);
-            } else {
-                head = nuevoNodo;
-            }
-            tail = nuevoNodo;
-    }
-   }
-   
-   void ImprimirLista(){
-    Nodo* aux = head;
-    while (aux != nullptr) {
-        cout << "Valor: " << aux->getValor() << " Peso: "<< aux->getPeso()<<endl;
-    }
-    cout<<endl;
-    cout<<endl;
-   }
-   
-    ListDoble(){
-        head = nullptr;
-        tail = nullptr;
-    }
-
-  
-};
-
-
-
-//se implementa el sistema de listas y nodos para tener una estructura de datops de una lista doblemente
-//ahora se creear una estructura vertice que almacenara una lista de nodos y un peso para cada uno de ellos
-
-class Vertice{
-private:
-int Id;
-char Tipo;
-ListDoble listaNodos;
-Vertice *next;
-Vertice *prev;
-public:
-
-   //getters
-   int getId(){ return Id; }
-   char getTipo(){ return Tipo; }
-   Vertice *getNext(){ return next; }
-   Vertice *getPrev(){ return prev; }
-
-   //setters 
-   void SetNext(Vertice*next){this->next=next;}
-   void SetPrev(Vertice*prev){this->prev=prev;}
-   void SetId(int Id){this->Id=Id;}
-   void SetTipo(char Tipo){this->Tipo=Tipo;}
-
-   
-   
-   
-   void CargarRelaciones(int Y,int Z){
-         listaNodos.InsertarAlFinal(Y,Z);
-   }
-   //constructor
-
-   Vertice(){
-    Id = 0;
-    listaNodos = ListDoble();
-    next = nullptr;
-    prev = nullptr;
-    Tipo = ' ';
-   }
-};
-
-class HECHIZO
-{
-private:
-    Vertice *head;
-    Vertice *tail;
-public:
-    //setters
-   
-
-    //getters
-    Vertice* getHead(){ return head; }
-    Vertice* getTail(){ return tail; }
-
-
-   bool YaAgregado(int X){
-    Vertice* aux = head;
-    while(aux != nullptr){
-        if(aux->getId() == X){
-
-            return true;
-        }
-        else{
-            aux = aux->getNext();
-        }
-    }
-    return false;
-   }
-   void InsertarAlFinal(int X){
-        if (!YaAgregado(X))
-        {
-               
-            Vertice* nuevoVertice = new Vertice();
-            nuevoVertice->SetId(X);
-            
-            nuevoVertice->SetNext(nullptr);
-            if (tail != nullptr) {
-                tail->SetNext(nuevoVertice);
-                nuevoVertice->SetPrev(tail);
-            } else {
-                head = nuevoVertice;
-            }
-            tail = nuevoVertice;
-        }
-        
-    
-    
-   }
-   
-
-    
-    //constructor
-    HECHIZO(){
-        head =nullptr;
-        tail =nullptr;
-        }
-   
-};
-
-//ya creado el sistema para almacenar los hechizos queda asignarselos a sus respectivos magos
-
-
-
-
-class Mago
-{
-private:
-    string Nombre;
-    HECHIZO Encantamiento;
-public:
-
-    Mago(){S
-        Encantamiento = HECHIZO();
-    }
-    
-};
-
 ifstream SpellList;
 
+//CADA VERTICE POSEE UNA LISTA DE LOS VERTICES ADYACENTES A EL
+struct Nodo{
+    int Id;
+    char Tipo;
+    float peso;
+    Nodo *siguiente;
+    void ImprimirDatos(){
+        cout << "Id: " << Id << endl;
+        cout << "Tipo: " << Tipo << endl;
+        cout << "Peso: " << peso << endl;
+        cout << "///---///---///---///---///---///---///"<<endl;
+        cout<<endl;
+    }
+    //CARACTER 0 CORRESPONDE AL VERTICE 1
+
+    Nodo(int Id,int peso,char tipo){
+        this->Id = Id;
+        this->Tipo = tipo;
+        this->peso = peso;
+    }
+    Nodo(){
+        Id = 0;
+        Tipo = ' ';
+        peso = 0;
+        siguiente = NULL;
+    }
+};
+struct Lista
+{
+    Nodo *primero;
+    Nodo *ultimo;
+    
+    bool YaAgregado(int Id){
+       Nodo *aux = primero;
+       while(aux != NULL){
+        if(aux->Id == Id){
+           return true;
+        }
+       }
+       return false;
+    }
+    void InsertarNodo(int Id,int peso,char tipo){
+        if (!YaAgregado(Id))
+        {
+            Nodo *nuevo = new Nodo(Id,peso,tipo);
+         
+            if (ultimo!=nullptr)
+            {
+                ultimo->siguiente = nuevo;
+            }else{
+                primero = nuevo;
+            }
+            ultimo = nuevo;
+        }
+        
+        
+        
+      
+    }
+    void MostrarLista(){
+      Nodo *aux = primero;
+      while (aux != nullptr){
+        aux->ImprimirDatos();
+        aux = aux->siguiente;
+
+      }
+    }
+    void EliminarItem(){
+        cout<<"Diga el id del elemento que quiera eliminar: ";
+        int id;
+        cin>>id;
+        Nodo *aux = primero;
+         Nodo *aux2;
+        while (aux->Id != id)
+        {   aux2 = aux;
+            aux =aux->siguiente;
+        }
+        
+        if (aux ==primero)
+        {
+            primero = primero->siguiente;
+            delete aux;
+        }
+        else{
+               aux2->siguiente = aux->siguiente;
+               delete aux;
+
+        }
+        
+    }
+
+  Lista(){
+    primero = nullptr;
+    ultimo = nullptr;
+  }
+
+};
+
+//IMPLEMENTACION DE LAS LISTA DE ADYACENCIA 
+struct Vertice
+{
+    int Id;
+    char Tipo;
+    Lista Adyacencia;
+    Vertice *siguiente;
+    void ImprimirDatos(){
+        cout << "Id: " << Id << endl;
+        cout << "Tipo: " << Tipo << endl;
+        cout << "Adyacencias: " ; Adyacencia.MostrarLista();  cout<<endl;
+        
+        cout << "///---///---///---///---///---///---///"<<endl;
+        cout<<endl;
+    }
+    //CARACTER 0 CORRESPONDE AL VERTICE 1
+    void CargarOrigen(int Origen,char Tipo){
+       Id = Origen;
+       this->Tipo = Tipo;
+    }
+    void CargarDatos(int Destino,float Peso,char Tipo){
+       Adyacencia.InsertarNodo(Destino,Peso,Tipo);
+    }
+    
+    Vertice(int Origen,char Tipo){
+        CargarOrigen(Origen,Tipo);
+    }
+    Vertice(){
+        Id = 0;
+        Tipo = ' ';
+        Adyacencia = Lista();
+        siguiente = NULL;
+    }
+};
+
+
+struct Hechizo
+{
+    Vertice *primero;
+    Vertice *ultimo;
+
+    bool YaAgregado(int Id){
+        Vertice *aux = primero;
+        while(aux != NULL){
+         if(aux->Id == Id){
+            return true;
+         }
+        }
+        return false;
+     }
+    void InsertarNodo(int Origen,char Tipo){
+        if (!YaAgregado(Origen))
+        {
+            Vertice *nuevo = new Vertice(Origen,Tipo);
+         
+            if (ultimo!=nullptr)
+            {
+              ultimo->siguiente = nuevo;
+            }else{
+                primero = nuevo;
+            }
+            ultimo = nuevo;
+        }
+        
+      
+        
+      
+    }
+    void CargarListasDeAdyacencia(int Origen,int Destino,float peso,char Tipo){
+        Vertice *aux =primero;
+        while(aux!=nullptr){
+            if (aux->Id ==Origen)
+            {
+                aux->CargarDatos(Destino,peso,Tipo);
+                
+            }
+            aux = aux->siguiente;
+            
+        }
+     }
+     void ImprimirListasDeAdyacencia(){
+        Vertice *aux = primero;
+        while(aux != NULL){
+            cout << "Vertice: " << aux->Id << endl;
+            cout << "Tipo: " << aux->Tipo << endl;
+            cout << "Peso con: ";
+            aux->ImprimirDatos();
+            cout<<endl;
+            aux = aux->siguiente;
+        }
+     }
+    
+    
+    void EliminarItem(){
+        cout<<"Diga el id del elemento que quiera eliminar: ";
+        int id;
+        cin>>id;
+        Vertice *aux = primero;
+         Vertice *aux2;
+        while (aux->Id != id)
+        {   aux2 = aux;
+            aux =aux->siguiente;
+        }
+        
+        if (aux ==primero)
+        {
+            primero = primero->siguiente;
+            delete aux;
+        }
+        else{
+               aux2->siguiente = aux->siguiente;
+               delete aux;
+
+        }
+        
+    }
+
+  Hechizo(){
+    primero=nullptr;
+    ultimo=nullptr;
+  }
+
+};
+
+struct Mago{
+
+    string Nombre;
+    int CantidadVertices;
+    string Caracteres;
+    int CantidadDeLineas;
+    Hechizo Encantamiento;
+    Mago *next;
+
+
+     
+     void LoadPrimary(string Nombre, string Caracteres, int CantidadVertices, int CantidadDeLineas){
+              this->Nombre = Nombre;
+              this->CantidadVertices = CantidadVertices;
+              this->Caracteres = Caracteres;
+              this->CantidadDeLineas = CantidadDeLineas;
+     }
+     void CargarPrincipales(int &Origen,int &Destino,float &Peso,string Arr[],int i){
+        string aux;
+        aux = Arr[i][0];
+        Origen = stoi(aux);
+        aux = Arr[i][1];
+        Destino = stoi(aux);
+        aux = Arr[i][2];
+        Peso = stoi(aux);
+     }
+     void CargarHechizo(string Arr[]){
+        
+        for (int i = 0; i < CantidadDeLineas; i++)
+        {   int Origen,Destino;
+            float Peso;
+            CargarPrincipales(Origen,Destino,Peso,Arr,i);
+            Encantamiento.InsertarNodo(Origen,Caracteres[Origen-1]); 
+        }
+
+        for (int i = 0; i < CantidadDeLineas; i++)
+        {   
+            int Origen,Destino;
+            float Peso;
+            CargarPrincipales(Origen,Destino,Peso,Arr,i);
+            Encantamiento.CargarListasDeAdyacencia(Origen,Destino,Peso,Caracteres[Destino-1]); 
+        }
+        
+     }
+     void ImprimirHechizo(){
+        Encantamiento.ImprimirListasDeAdyacencia();
+     }
+
+     Mago(){
+        Nombre = "";
+        CantidadVertices = 0;
+        Caracteres = "";
+        next = nullptr;
+        CantidadDeLineas = 0;
+        Encantamiento = Hechizo();
+    }
+};
+
+
+
+
+
+
+
+int NumeroHechizos(){
+    SpellList.open("spellList.in", ios::in);
+    if (SpellList.fail()) {
+        cout << "Error al abrir el archivo" << endl;
+        return 0;
+    }
+    string line;
+    getline(SpellList, line);
+    
+    SpellList.close();
+    
+    return stoi(line);
+}
+
+
+
 int main(){
+int N = NumeroHechizos();
+
 
     return 0;
-}DDDDD
+}
