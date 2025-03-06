2231db0d2d1987b75aef07f8a98906261673e551
Jorge Arias <ja549655@gmail.com>
2025-03-06 15:00:23 -0400

    Ahora si la lista para el control de grafos funciona

--- GestionArcana.cpp
+++ GestionArcana.cpp
@@ -147,46 +147,41 @@ bool NodoAAGREGADO(int ID){
   }
   return false;
 }
+void AgregarRelacion(int NodoOrigen, int NodoDestino, char NombreOrigen, char NombreDestino, float Peso) {
+  if (!NodoAAGREGADO(NodoOrigen)) {
+      NodoV2* nuevo = new NodoV2();
+      nuevo->SetNodoA(NodoOrigen);
+      nuevo->SetNombre(NombreOrigen);
+      nuevo->SetAdyacencia(NodoDestino, NombreDestino, Peso);
+
+      NodoV2* aux1 = first;
+      NodoV2* aux2 = nullptr;
+      while ((aux1 != nullptr) && (aux1->GetNodoA() < nuevo->GetNodoA())) {
+          aux2 = aux1;
+          aux1 = aux1->GetNext();
+      }
+      if (aux1 == first) {
+          first = nuevo;
+      } else {
+          aux2->SetNext(nuevo);
+      }
+      nuevo->SetNext(aux1);
+      nuevo->SetPrev(aux2);
+  } else {
+      NodoV2* aux = first;
+      while (aux->GetNodoA() != NodoOrigen) {
+          aux = aux->GetNext();
+      }
+      aux->SetAdyacencia(NodoDestino, NombreDestino, Peso);
+  }
+}
 
 void AddNodoA(int NodoA,int NodoB,char NombreA,char NombreB,float Peso){
-  if (!NodoAAGREGADO(NodoA))
-  {
-    NodoV2 *nuevo = new NodoV2();
-    nuevo->SetNodoA(NodoA);
-    nuevo->SetNombre(NombreA);
-    nuevo->SetAdyacencia(NodoB,NombreB,Peso);
   
-    NodoV2* aux1 = first;
-    NodoV2* aux2;
-    while ((aux1 !=nullptr) && (aux1->GetNodoA() < nuevo->GetNodoA()))
-    {
-      aux2 = aux1;
-      aux1 = aux1->GetNext();
-    }
-    if (aux1 == first){ //es el primero
-      first = nuevo;
-    }
-    else{
-      aux2->SetNext(nuevo);
-    }
-    nuevo->SetNext(aux1);
-    nuevo->SetPrev(aux2);
-  }
-  else{
-    // cout<<"YA AGREGADO"<<endl;
-
-    NodoV2 *aux = first;
-           while (aux->GetNodoA() != NodoA)
-           {
-               aux = aux->GetNext();
-           }
-           // Verificar si la adyacencia ya existe antes de agregar
-           // Aquí se puede agregar lógica para evitar duplicados
-           aux->SetAdyacencia(NodoB, NombreB, Peso);     
-
-  }
-  
-  
+  // Añadir NodoA -> NodoB
+  AgregarRelacion(NodoA, NodoB, NombreA, NombreB, Peso);
+  // Añadir NodoB -> NodoA
+  AgregarRelacion(NodoB, NodoA, NombreB, NombreA, Peso);
 }
   
 void ImprimirListaV2(){
@@ -223,10 +218,15 @@ public:
 
 int main(){
   ListaV2 test;
-  test.AddNodoA(1,2,'a','b',5);
-  test.AddNodoA(2,7,'c','d',7);
-  test.AddNodoA(2,9,'e','f',3);
-  test.AddNodoA(1,3,'e','f',3);
+  string Letras = "ABFBDB";
+  test.AddNodoA(6,1,Letras[6-1],Letras[1-1],2);
+  test.AddNodoA(6,4,Letras[6-1],Letras[4-1],1);
+  test.AddNodoA(1,2,Letras[1-1],Letras[2-1],1);
+  test.AddNodoA(1,4,Letras[1-1],Letras[4-1],4);
+  test.AddNodoA(2,4,Letras[2-1],Letras[4-1],7);
+  test.AddNodoA(2,5,Letras[2-1],Letras[5-1],2);
+  test.AddNodoA(5,3,Letras[5-1],Letras[3-1],1);
+  test.AddNodoA(4,3,Letras[4-1],Letras[3-1],3);
   //el nodo 1 deberia estar relacionado con los nodos 2 7 9  3
 
   test.ImprimirListaV2();
